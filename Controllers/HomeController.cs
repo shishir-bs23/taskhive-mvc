ad36d962ea08c9f98393af28412aa493f8aa8f04
Shishir <hasanur.shishir@brainstation-23.com>
2025-08-21 15:52:06 +0600

    implement searching and sorting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using TaskHive.Models;
 
 namespace TaskHive.Controllers;
@@ -25,9 +26,48 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Tasks()
+    // public IActionResult Tasks(string searchString)
+    // {
+    //     var allTasks = _context.Tasks.ToList();
+    //     if (!String.IsNullOrEmpty(searchString))
+    //     {
+    //         allTasks = allTasks.Where(n => n.TaskTitle.Contains(searchString)).ToList();
+    //     }
+    //     return View(allTasks);
+    // }
+
+    public IActionResult Tasks(string searchString, string sortOrder)
     {
         var allTasks = _context.Tasks.ToList();
+        if (!String.IsNullOrEmpty(searchString))
+        {
+            allTasks = allTasks.Where(n => n.TaskTitle.Contains(searchString)).ToList();
+        }
+        allTasks = allTasks.OrderBy(e => e.TaskTitle).ToList();
+        allTasks = allTasks.OrderByDescending(e => e.TaskTitle).ToList();
+
+        switch (sortOrder)
+        {
+
+            case "id_asc":
+                allTasks = allTasks.OrderBy(e => e.TaskId).ToList();
+                break;
+
+            case "title_asc":
+                allTasks = allTasks.OrderBy(e => e.TaskTitle).ToList();
+                break;
+
+            case "creation_date_asc":
+                allTasks = allTasks.OrderBy(e => e.TaskCreationDate).ToList();
+                break;
+            case "due_date_asc":
+                allTasks = allTasks.OrderBy(e => e.TaskDueDate).ToList();
+                break;
+            default:
+                allTasks = allTasks.OrderBy(e => e.TaskId).ToList();
+                break;
+       }
+
         return View(allTasks);
     }
 
@@ -35,10 +75,10 @@ public class HomeController : Controller
     {
         if (id != null)
         {
-            var taskInDb =_context.Tasks.SingleOrDefault(task => task.TaskId == id);
+            var taskInDb = _context.Tasks.SingleOrDefault(task => task.TaskId == id);
             return View(taskInDb);
         }
-       
+
         return View();
     }
 
@@ -48,6 +88,12 @@ public class HomeController : Controller
         _context.Tasks.Remove(taskInDb);
         _context.SaveChanges();
         return RedirectToAction("Tasks");
+    }
+
+    public IActionResult Details(int id)
+    {
+        var taskInDb = _context.Tasks.SingleOrDefault(task => task.TaskId == id);
+        return View(taskInDb);
     }
 
     public IActionResult CreateForm(TaskModel model)
@@ -60,7 +106,7 @@ public class HomeController : Controller
         {
             _context.Tasks.Update(model);
         }
-       
+
         _context.SaveChanges();
         return RedirectToAction("Tasks");
     }
